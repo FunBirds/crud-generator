aa767d3f9a018b16a9f2da225abec8ad54082e14
Abdullajonov Boburjon <abdullajonovboburjon05@gmail.com>
2024-12-14 10:46:08 +0500

    [first commit][testing]

---
+++ src/Console/MakeCrud.php
@@ -0,0 +1,67 @@
+<?php
+namespace Foxdevuz\CrudGenerator\Console;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Str;
+use Symfony\Component\Console\Command\Command as CommandAlias;
+
+class MakeCrud extends Command
+{
+    protected $signature = 'make:crud
+        {--c|controller=: Controller name}
+        {--m|model=: Model name}';
+
+    protected $description = 'Generate a CRUD Controller bound to a Model';
+
+    public function handle()
+    {
+        $controllerName = $this->option('controller');
+        $modelName = $this->option('model');
+
+        // Validate inputs
+        if (!$controllerName || !$modelName) {
+            $this->error('Both --controller and --model options are required.');
+            return CommandAlias::FAILURE;
+        }
+
+        // Ensure proper naming conventions
+        $controllerName = Str::studly($controllerName . 'Controller');
+        $modelName = Str::studly($modelName);
+
+        // Resolve stub path
+        $stubPath = __DIR__ . '/../stubs/crud.controller.stub';
+        if (!File::exists($stubPath)) {
+            $this->error('CRUD controller stub not found.');
+            return CommandAlias::FAILURE;
+        }
+
+        // Read stub content
+        $stub = File::get($stubPath);
+
+        // Replace placeholders
+        $controllerContent = str_replace(
+            ['{{controllerName}}', '{{modelName}}', '{{namespace}}'],
+            [$controllerName, $modelName, 'App\Http\Controllers'],
+            $stub
+        );
+
+        // Determine controller file path
+        $controllerPath = app_path("Http/Controllers/{$controllerName}.php");
+
+        // Check for existing file
+        if (File::exists($controllerPath)) {
+            $this->error("Controller {$controllerName} already exists.");
+            return CommandAlias::FAILURE;
+        }
+
+
+        File::ensureDirectoryExists(dirname($controllerPath));
+
+        // Write controller file
+        File::put($controllerPath, $controllerContent);
+
+        $this->info("Controller {$controllerName} created successfully!");
+        return CommandAlias::SUCCESS;
+    }
+}
---
+++ src/CrudGeneratorServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+namespace Foxdevuz\CrudGenerator;
+
+use Illuminate\Support\ServiceProvider;
+use Foxdevuz\CrudGenerator\Console\MakeCrud;
+
+class CrudGeneratorServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        // Register the Artisan command
+        $this->commands([
+            MakeCrud::class,
+        ]);
+    }
+
+    public function boot()
+    {
+        // Publish the stubs so users can customize them
+        $this->publishes([
+            __DIR__ . '/stubs/crud.controller.stub' => resource_path('stubs/crud.controller.stub'),
+        ], 'crud-generator-stubs');
+    }
+}
